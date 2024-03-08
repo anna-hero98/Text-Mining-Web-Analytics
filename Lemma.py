0c32335b2dbda22b738145e98d54760c999067a8
Luisa700 <157422091+Luisa700@users.noreply.github.com>
2024-03-08 11:29:10 +0100

    Lemma auf Data frame

--- Lemma.py
+++ Lemma.py
@@ -1,29 +1,43 @@
+import collections
 import requests
 import spacy
 from collections import Counter
+from wordcloud import WordCloud
+import matplotlib. pyplot as plt
 
 nlp = spacy.load("de_core_news_sm")
 headers = {'Accept': 'application/json', 'Authorization': 'ApiKey rgsaY4U.oZRQKUHdJhF9qguHMkwCGIoLaqEcaHjYLF'}
 
-Dokumentnr = input('Bitte Dokumentnummer eingeben: ')
-url = f'https://search.dip.bundestag.de/api/v1/plenarprotokoll-text?f.dokumentnummer={Dokumentnr}'
-response = requests.get(url, headers=headers)
+import pandas as pd
+import spacy
+from collections import Counter
 
-json_data = response.json()
+# Laden des deutschen Sprachmodells von Spacy
+nlp = spacy.load('de_core_news_sm')
+nlp.max_length = 49027203
 
-if 'documents' in json_data:
-    for document in json_data['documents']:
-        text = document['text']
+# Laden der CSV-Datei
+data = pd.read_csv("extracted_data.csv").head(n=300).dropna()
 
-        # spaCy-Verarbeitung
-        doc = nlp(text)
 
-        # Lemmatisierung und Frequenzzählung
-        lemmas = [token.lemma_.lower() for token in doc if token.is_alpha and not token.is_stop]
-        lemma_freq = Counter(lemmas)
+# Alle Texte aus der Spalte "Text" zu einem einzigen Text zusammenführen
+text = " ".join(data['Text'])
 
-        # Sortieren der Frequenzen in absteigender Reihenfolge
-        for lemma, freq in lemma_freq.most_common():
-            print(f"{lemma}: {freq}")
+# spaCy-Verarbeitung
+doc = nlp(text)
+
+# Lemmatisierung und Frequenzzählung
+lemmas = [token.lemma_.lower() for token in doc if token.is_alpha and not token.is_stop]
+lemma = collections.Counter(lemmas)
+lemma_freq = Counter(lemmas)
+
+wc = WordCloud().generate_from_frequencies(lemma_freq)
+plt.imshow(wc)
+plt.show()
+
+# Sortieren der Frequenzen in absteigender Reihenfolge
+for lemma, freq in lemma_freq.most_common():
+    print(f"{lemma}: {freq}")
 else:
     print("Keine Dokumente gefunden.")
+
