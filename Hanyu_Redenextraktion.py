dd00fe5aa679bd7b37dc702b965b9020c4d3876d
anna-hero98 <anna.hero@web.de>
2024-03-10 13:28:05 +0100

    alles aktualisiert

--- Hanyu_Redenextraktion.py
+++ Hanyu_Redenextraktion.py
@@ -3,13 +3,13 @@ import pandas as pd
 import re
 import subprocess
 
-
-# parteien = {"(CDU/CSU):","(DIE LINKE):","(BÜNDNIS 90/DIE GRÜNEN):", "(SPD):", "(FDP):", "(AfD):"}
-filepath = 'input_csv.csv'
+filepath = 'testliste.csv'
 df = pd.read_csv(filepath, sep=';')
 #Zeilenumbrüche bei Titel rausfiltern
 df.replace('\n', '', regex=True, inplace=True)
 extracted_df = pd.DataFrame(columns=['Jahr', 'Dokumentnr', 'Name', 'Partei', 'Thema', 'Titel', 'Text'])
+
+bsp_df = pd.DataFrame(columns=['Jahr', 'Dokumentnr', 'Name', 'Partei', 'Thema', 'Titel', 'Text'])
 
 for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokumentnr'], df['Name'], df['Partei'],
                                                             df['Thema'], df['Titel']):
@@ -27,27 +27,30 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
                 text = document['text']
                 # Ersetzt ' – ' mit ' - '
                 text = re.sub(r'–', '-', text)
-                # Entfernt Bindestrich nachdem ein Zeilenumbruch kommt
-                text = re.sub(r'-\n\s*', '-', text)
                 # Entfernt Zeilenumbrüche und nicht-sichtbare Leerzeichen
                 text = re.sub(r'\s+', ' ', text.replace('\n', ' ')).strip()
+                # Entfernt Leerzeichen, das nach Bindestrich kommt kommt
+                text = re.sub(r'\w-\s*', '-', text)
 
                 # Gesuchte Wortreihenfolge und Text nachfolgend bis zu einer anderen Wortfolge
                 search_sentence = titel
 
                 # Split the sentence into words
                 words = search_sentence.split()
-                # Extract the first two words and the last word
-                shortened_title = " ".join(words[2:-1])
+
+                # Initialize shortened_title with the original search_sentence
+                shortened_title = search_sentence
                 print(shortened_title)
 
-                # Shorten the last word by 10 letters
-                if len(words[-1]) > 10:
-                    shortened_last_word = words[-1][:-10]
-                    shortened_title += " " + shortened_last_word
-            else:
-                # If the last word is already shorter than 10 letters, keep it unchanged
-                shortened_title += " " + words[-1]
+                if len(words) > 6:
+                    # Extract the first two words and the last 3 word
+                    shortened_title = " ".join(words[2:-2])
+                    print(shortened_title)
+
+                if shortened_title in text:
+                    print("Titel gefunden")
+                else:
+                    print("Titel nicht gefunden")
 
                 # Suchen nach dem zweiten Vorkommen des Titels
                 titel_index = text.find(shortened_title, text.find(shortened_title) + 1)
@@ -59,7 +62,6 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
                 # Add newline character after each sentence
                 result_new = '\n'.join(sentences)
 
-
                 ende = "):"
                 rede = ""
 
@@ -69,20 +71,9 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
                     print("Name + Partei")
                     start = f"{name} ({partei}):"
                     print(start)
-                    zwischenfrage = "Zwischenfrage"
 
-                    # Suchen Sie nach dem ersten Vorkommen von "start"
-                    start_index = result_new.find(start)
-
-                    # Überprüfen Sie, ob "start" im Text gefunden wurde
-                    if start_index != -1:
-                        # Suchen Sie nach dem letzten Vorkommen von "Zwischenfrage" vor dem ersten Vorkommen von "start"
-                        search_range = result_new[max(0, start_index - 30):start_index]
-                        zwischenfrage_index = search_range.rfind(zwischenfrage)
-
-                        # Suchen Sie nach dem zweiten Vorkommen von "start"
-                        if zwischenfrage_index != -1:
-                            start_index = result_new.find(start, start_index)
+                    start_index = result_new.find(start, result_new.find(start))
+                    start_index += len(start)
 
                 elif start != result_new:
                     print("Name + Zusatz + Partei")
@@ -91,7 +82,7 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
                     regex_match = re.compile(start)
                     match = re.findall((regex_match), result_new)
                     newstart = "".join(match)  # re.findall Ergebnis -> Liste, diese in string umwandeln
-                    #print(newstart)
+                    print(newstart)
 
                     start_index = result_new.find(newstart, result_new.find(newstart))
                     start_index += len(newstart)
@@ -102,17 +93,15 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
                     regex_match = re.compile(start)
                     match = re.findall((regex_match), result_new)
                     newstart = "".join(match)  # re.findall Ergebnis -> Liste, diese in string umwandeln
-                    #print(newstart)
+                    print(newstart)
 
                     start_index = result_new.find(start, result_new.find(start))
                     start_index += len(newstart)
 
-
-
-                end_position = result.find(ende, start_index)
+                end_position = result_new.find(ende, start_index)
                 if ende != -1:
-                    rede = result[start_index:end_position].strip()
-                    # print(rede)
+                    rede = result_new[start_index:end_position].strip()
+                    rede = rede.replace(';', r'')  #Nimmt Semikolon raus, damit es nicht zu Konflikten mit dem sep=";" kommt
                     break
 
         extracted_df = extracted_df._append(
@@ -122,7 +111,7 @@ for jahr, dokumentnr, name, partei, thema, titel in zip(df['Jahr'], df['Dokument
     else:
         print("Error fetching data for dokumentnr:", dokumentnr)
 print(extracted_df)
-extracted_df.to_csv('extracted_data.csv', index=False)
+extracted_df.to_csv('extracted_data.csv', sep = "*", index=False)
 
 csv_file = 'extracted_data.csv'
 
