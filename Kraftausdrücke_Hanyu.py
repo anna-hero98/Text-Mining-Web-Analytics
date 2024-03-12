ba81fc62f8cd5c3ca2ac173a64d9579a0ff40d2f
JJEinert <hanni.einert@gmail.com>
2024-03-13 00:06:32 +0100

    Performantere Kraftausdruck Auswertung, ca. 90sekunden Berechnungszeit

--- Kraftausdrücke_Hanyu.py
+++ Kraftausdrücke_Hanyu.py
@@ -1,7 +1,6 @@
 import spacy
 import nltk
 import pandas as pd
-
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 
@@ -33,25 +32,27 @@ for row in kraftausdruecke_data['Kraftausdruecke']:
 anzahl_treffer = 0
 
 for idx, (jahr, partei, thema, text) in enumerate(zip(data['Jahr'], data['Partei'], data['Thema'], data['Text'])):
-    # Überprüfen, ob eines der Kraftausdrücke in der Zeile vorkommt
-    for kraftausdruck in kraftausdruecke_list:
-        if kraftausdruck in text:
-            sentences = punkt_tokenizer.tokenize(text)
-            print(f"**{kraftausdruck}** gefunden in Zeile: {idx}\n Jahr: {jahr} Partei: {partei}  Thema: {thema}")
-            anzahl_treffer += 1
-            idx_list.append(idx)
-            for sentence in sentences:
-                #sent_nlp = nlp(sentence)
-                if kraftausdruck in sentence:
-                    highlighted_sentence = sentence.replace(kraftausdruck, f"**{kraftausdruck}**")  # Wenn Karftausdruck im Text wird das Wort durch **word** ersetzt um es hervorzuheben
-                    highlighted_sentence = highlighted_sentence, kraftausdruck
-                    hs_nlp = nlp(highlighted_sentence[0])
-                    sentiment_scores = sia.polarity_scores(hs_nlp.text)  # Wenn Kraftausdruck vorliegt, wird eine Sentiemntanaylse durchgeführt
-                    # sia ist ein Sentiment Intensity Analyzer Tool, das die Polarität des Sentiments (positiv, neutral, negativ) und die Intensität des Sentiments im Satz berechnet
-                    print(f"Satz: {hs_nlp.text}")
-                    print(f"Sentiment-Scores: {sentiment_scores}")
-                    print(f"Wort:{kraftausdruck}\n")
-
+    if isinstance(text, str):
+        # Überprüfen, ob eines der Kraftausdrücke in der Zeile vorkommt
+        for kraftausdruck in kraftausdruecke_list:
+            if kraftausdruck in text:
+                sentences = punkt_tokenizer.tokenize(text)
+                print(f"**{kraftausdruck}** gefunden in Zeile: {idx}\n Jahr: {jahr} Partei: {partei}  Thema: {thema}")
+                anzahl_treffer += 1
+                idx_list.append(idx)
+                for sentence in sentences:
+                    #sent_nlp = nlp(sentence)
+                    if kraftausdruck in sentence:
+                        highlighted_sentence = sentence.replace(kraftausdruck, f"**{kraftausdruck}**")  # Wenn Karftausdruck im Text wird das Wort durch **word** ersetzt um es hervorzuheben
+                        highlighted_sentence = highlighted_sentence, kraftausdruck
+                        hs_nlp = nlp(highlighted_sentence[0])
+                        sentiment_scores = sia.polarity_scores(hs_nlp.text)  # Wenn Kraftausdruck vorliegt, wird eine Sentiemntanaylse durchgeführt
+                        # sia ist ein Sentiment Intensity Analyzer Tool, das die Polarität des Sentiments (positiv, neutral, negativ) und die Intensität des Sentiments im Satz berechnet
+                        print(f"Satz: {hs_nlp.text}")
+                        print(f"Sentiment-Scores: {sentiment_scores}")
+                        print(f"Wort:{kraftausdruck}\n")
+    else:
+        print("Für text keinen str erkannt")
 
 
 
